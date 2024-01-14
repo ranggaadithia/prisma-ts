ac8a2e9ca87f95c391bfce7b744b197816ca3a34
Rangga Adithia <rangga.adithia26@gmail.com>
2024-01-14 13:51:07 +0800

    fixing res status

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,16 +3,15 @@ import express, { Request, Response } from "express";
 const prisma = new PrismaClient();
 
 class UserController {
-  async index(req: Request, res: Response) {
-   try {
-        const users = await prisma.user.findMany();
-        res.status(200);
-        res.json(users);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-      }
+ async index(req: Request, res: Response) {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
+ }
 
   async create(req: Request, res: Response) {
     try {
@@ -24,8 +23,7 @@ class UserController {
           username
         }
       });
-      res.status(200);
-      res.json(user);
+      res.status(200).json(user);
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Internal Server Error' });
